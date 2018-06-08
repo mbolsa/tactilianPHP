d73605d097750aaa72b7baf388e880dcaaa784d7
jorgeaguaron <jorge@aguaron.com>
2018-06-08 09:29:31 +0200

    Estructura

---
+++ cabecera.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("conexion.inc.php");
+$conexion = new mysqli($servidor, $usuario, $passwd, $basedatos);
+if (mysqli_connect_errno())
+{
+	echo "Error al establecer la conexión con la base de datos: " . mysqli_connect_error();
+	exit();
+}
+$conexion->query("set names utf8");
+
+
+?>
+
+<html>
+<head>
+
+<title>
