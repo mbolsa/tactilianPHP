77418d9888b4d9d7a19e9f3fe6348bc2f4fa59e3
mbolsa <miguellbolsa@gmail.com>
2018-06-14 00:08:39 +0200

    lista de actividades

--- activities.php
+++ activities.php
@@ -1,3 +1,17 @@
+<?php
+
+session_start();
+require_once("conexion.inc.php");
+$conexion = new mysqli($servidor, $usuario, $passwd, $basedatos);
+if (mysqli_connect_errno())
+{
+  echo "Error al establecer la conexión con la base de datos: " . mysqli_connect_error();
+  exit();
+}
+
+$conexion->query("set names utf8");
+
+?>
 <html>
   <head>
 	<title>Choose Activity</title>
@@ -7,33 +21,30 @@
 
 
 
-  <br>
-  <br>
+  <br></br>
+  <br></br>
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-5">
         <div class="form-group">
           <h2> Seleccione Actividad </h2>
-          <select multiple class="form-control" size="10">
-            <option> Actividad generica 1 </option>
-            <option> Actividad generica 2 </option>
-            <option> Actividad generica 3 </option>
-            <option> Actividad generica 4 </option>
-            <option> Actividad generica 5 </option>
-            <option> Actividad generica 6 </option>
-            <option> Actividad generica 7 </option>
-            <option> Actividad generica 8 </option>
-            <option> Actividad generica 9 </option>
-            <option> Actividad generica 10 </option>
-            <option> Actividad generica 11 </option>
-            <option> Actividad generica 12 </option>
-            <option> Actividad generica 13 </option>
-            <option> Actividad generica 14 </option>
-            <option> Actividad generica 15 </option>
-            <option> Actividad generica 16 </option>
-            <option> Actividad generica 17 </option>
-            <option> Actividad generica 18 </option>
-          </select>
+          <?php
+          $activities = $conexion->query("select id, name from genericActivity");
+          if ($activities->num_rows == 0)
+          {
+            echo "<p><b>No hay actividades creadas</b></p>";
+          }
+          else {
+            ?>
+            <select class="form-control" size="10" id="chooser">
+            <?php
+            while ($activity = $activities->fetch_array())
+            {
+              echo "<option value='" . $activity[0] . "'>$activity[1]</option>";
+            }
+            echo "</select>";
+          }
+          ?>
         </div>
         <br>
       </div>
@@ -41,12 +52,10 @@
       <div class="col-md-2"></div>
       <div class="col-md-4">
         <br>
-        <br>
-        <br>
         <div class="wrapper">
           <div class="text-center">
             <span class="group-btn btn-group-justified ">
-              <a href="/chooseStu" class="btn btn-light btn-block"> Iniciar </a>
+              <a href="#" class="btn btn-light btn-block"> Iniciar </a>
             </span>
           </div>
         </div>
